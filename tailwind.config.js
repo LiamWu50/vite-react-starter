be4236a114d3d46424cd69608393e54606b78c08
wulin <admin@MacBook.local>
2023-10-18 16:46:26 +0800

    build: update build options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-module.exports = {
-  content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {}
   },
